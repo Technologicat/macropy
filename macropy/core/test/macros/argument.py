92ead91d8c452569f225a27a82ae857e9bdf7705
Juha Jeronen <juha.jeronen@jyu.fi>
2019-05-15 15:34:18 +0300

    add test for new named macro args mechanism

--- macropy/core/test/macros/argument.py
+++ macropy/core/test/macros/argument.py
@@ -1,8 +1,10 @@
-from macropy.core.test.macros.argument_macros import macros, expr_macro, block_macro, decorator_macro
+from macropy.core.test.macros.argument_macros import macros, expr_macro, block_macro, decorator_macro, expr_macro_with_named_args
 import math
 
 def run():
     x = expr_macro(1 + math.sqrt(5))[10 + 10 + 10]
+
+    x = expr_macro_with_named_args(1 + 2 + 3, a=(1 + math.sqrt(5)))[10 + 10 + 10]
 
     with block_macro(1 + math.sqrt(5)) as y:
         x = x + 1
--- macropy/core/test/macros/argument_macros.py
+++ macropy/core/test/macros/argument_macros.py
@@ -4,6 +4,13 @@ import macropy.core.macros
 macros = macropy.core.macros.Macros()
 
 @macros.expr
+def expr_macro_with_named_args(tree, args, kwargs, **kw):
+    assert "a" in kwargs, kwargs
+    assert macropy.core.unparse(kwargs["a"]) == "(1 + math.sqrt(5))", macropy.core.unparse(kwargs["a"])
+    assert list(map(macropy.core.unparse, args)) == ["((1 + 2) + 3)"], macropy.core.unparse(args)
+    return tree
+
+@macros.expr
 def expr_macro(tree, args, **kw):
     assert list(map(macropy.core.unparse, args)) == ["(1 + math.sqrt(5))"], macropy.core.unparse(args)
     return tree
