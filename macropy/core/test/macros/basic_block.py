fb4370548194e998dc4eab07b74321f8daf39b86
Juha Jeronen <juha.jeronen@jyu.fi>
2019-05-15 16:38:44 +0300

    add test for multiple block macros in the same 'with', for the case where the inner macro returns a completely new tree

--- macropy/core/test/macros/basic_block.py
+++ macropy/core/test/macros/basic_block.py
@@ -1,7 +1,19 @@
-from macropy.core.test.macros.basic_block_macro import macros, my_macro
+from macropy.core.test.macros.basic_block_macro import macros, my_macro, my_nested_outer, my_nested_inner
 
 def run():
     x = 10
     with my_macro as y:
         x = x + 1
+
+    z = 10
+    with my_nested_outer:
+        with my_nested_inner:
+            z = z + 1  # should get replaced by z = z * 2
+    assert z == 80
+
+    z = 10
+    with my_nested_inner, my_nested_outer:
+        z = z + 1
+    assert z == 80
+
     return x
--- macropy/core/test/macros/basic_block_macro.py
+++ macropy/core/test/macros/basic_block_macro.py
@@ -8,3 +8,12 @@ def my_macro(tree, target, **kw):
     assert macropy.core.unparse(target) == "y"
     assert macropy.core.unparse(tree).strip() == "x = (x + 1)", macropy.core.unparse(tree)
     return tree * 3
+
+@macros.block
+def my_nested_outer(tree, **kw):
+    assert macropy.core.unparse(tree).strip() == "z = (z * 2)", macropy.core.unparse(tree)
+    return tree * 3
+
+@macros.block
+def my_nested_inner(tree, **kw):  # important: generate new tree from scratch for this test
+    return [macropy.core.parse_stmt("z = z * 2")]
